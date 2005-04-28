fa4d04c129add45aaff1524e8c7e4f534e170a4a
whit537 <whit537@b631e45e-7f1d-0410-89c3-4f15477e85a6>
2005-04-28 18:23:17 +0000

    git-svn-id: https://testosterone.googlecode.com/svn/pytest/trunk@11 b631e45e-7f1d-0410-89c3-4f15477e85a6

--- example.py
+++ example.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env pytest
-from pytest import utils
+# this works ^^^^^^^^
+
+"""
+
+Pytest is a testing framework for Python. This tutorial will show you how to use
+it. In general, testing is made up of three things:
+
+    1. fixture -- the thing you want to test
+
+    2. tests -- things you do to your fixture to see if it behaves as expected
+
+    3. reports -- tests are worthless with their results are communicated to you
+
+Other testing frameworks such as PyUnit (which is included in the standard
+library as unittest) and Brah Cohen's testtest module
+
 
 ##
-# the basics
+# The basics
 ##
 
 # passing tests are silent; no output is reported
-1+1==2
+1 + 1 == 2
 
 # a failing test produces output
-1+1==3
+1 + 1 == 3
 
 # print statements also produce output
 print 'hello world!'
@@ -20,6 +35,11 @@ pprint('look ma! no burdensome framework!')
 
 # in fact, all writes to stdout will be included in our report
 sys.stdout.write('teehee!')
+
+
+
+
+from pytest import utils
 
 
 def foo():
